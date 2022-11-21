79e6477a1c9334915928469cf903e05d2f715741
bahadauriaabhay <abhay.bahadauria@tothenew.com>
2022-11-22 02:11:16 +0530

    new

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world push 20 ! ');
+  res.send('Hello world push 21 ! ');
 });
 
 app.listen(PORT, HOST);
